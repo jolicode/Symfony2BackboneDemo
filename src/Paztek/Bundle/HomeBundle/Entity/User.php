62f3eaf94ea275a63076bf05c96d0f509f0c56fd
paztek <mbalmes@jolicode.com>
2013-03-08 14:05:11 +0100

    Initial setup + config of FOSUserBundle

---
+++ src/Paztek/Bundle/HomeBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Paztek\Bundle\HomeBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="Paztek\Bundle\HomeBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
---
+++ src/Paztek/Bundle/HomeBundle/PaztekHomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Paztek\Bundle\HomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PaztekHomeBundle extends Bundle
+{
+}
